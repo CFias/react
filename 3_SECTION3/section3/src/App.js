d86df64a1fea0aa6cf64d738e6b91aae109ed7db
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-19 17:15:48 -0300

    Update App.js

--- 3_SECTION3/section3/src/App.js
+++ 3_SECTION3/section3/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import City from './assets/city.jpg'
 
 function App() {
   return (
     <div className="App">
         <h1>Iniciando a seção 3</h1>
+          {/* Imagem em public */}
         <div>
           <img src="/img1.jpg" alt="Paisagem" />
+        </div>
+          {/* Imagem em asset */}
+        <div>
+          <img src={City} alt="Cidade" />
         </div>
     </div>
   );
