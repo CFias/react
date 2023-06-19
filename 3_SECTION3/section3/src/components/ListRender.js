433f6de223cc7d1ff5a4f65c3b819a181d61a79f
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-19 18:06:06 -0300

    Update ListRender.js

--- 3_SECTION3/section3/src/components/ListRender.js
+++ 3_SECTION3/section3/src/components/ListRender.js
@@ -3,11 +3,25 @@ import {useState} from 'react'
 const ListRender = () => {
     const [list] = useState(["Cleidson", "Everaldo", "Milena", "Celia"])
 
+    const [users] = useState([
+        { id: 12345, name: "Cleidson", age: 25 },
+        { id: 23456, name: "Ramos", age: 25 },
+        { id: 34567, name: "Fias", age: 25 },
+        { id: 45678, name: "Milena Fias", age: 25 },
+    ]);
+
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
+            ))}
+        </ul>
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>
+                    {user.name} - {user.age}
+                </li>
             ))}
         </ul>
     </div>
