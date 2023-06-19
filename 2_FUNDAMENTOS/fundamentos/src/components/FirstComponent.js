c2fa4d24f84b459aa8184fc9757fb41a23889345
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-19 13:34:41 -0300

    Create FirstComponent

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,19 @@
+// arquivo de estilo
+
+const FirstComponent = () => {
+    // essa função faz isso
+
+    /*
+        multi lines
+    */
+
+
+    return (
+        <div>
+            {/* Algum comentário */}
+            <h1>Este é o meu primeiro componente</h1>
+        </div>
+    )
+}
+
+export default FirstComponent;
