79118c8516abfd3d832d84e6415be7420f88cf46
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-19 13:34:23 -0300

    Update App.js

--- 1_INTRO/hello-world/src/App.js
+++ 1_INTRO/hello-world/src/App.js
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World React!</h1>
+      <h2>Emmet funcionou</h2>
+      <p>Desafio One</p>
     </div>
   );
 }
+
 
 export default App;
