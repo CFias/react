5b4d7a73dbe99272ea7f5322bad1775361cfb72d
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-19 17:09:07 -0300

    Update App.js

--- 3_SECTION3/section3/src/App.js
+++ 3_SECTION3/section3/src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
         <h1>Iniciando a seção 3</h1>
+        <div>
+          <img src="/img1.jpg" alt="Paisagem" />
+        </div>
     </div>
   );
 }
