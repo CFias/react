8185cb7031804ff69fe6307a6dfddb6206e38e6c
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-23 12:08:32 -0300

    Update ShowUserName.js

--- 3_SECTION3/section3/src/components/ShowUserName.js
+++ 3_SECTION3/section3/src/components/ShowUserName.js
@@ -1,9 +1,9 @@
 const ShowUserName = (props) => {
   return (
     <div>
-        <h2>O nome do usuário é: {props.name}</h2>
+      <h2>O nome do usuário é: {props.name}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default ShowUserName
+export default ShowUserName;
