7a2d5af2c6eb7bdd0bbd724c3950e61b350d6667
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-22 23:08:53 -0300

    Update App.js

--- 3_SECTION3/section3/src/App.js
+++ 3_SECTION3/section3/src/App.js
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import City from './assets/city.jpg'
 import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
+import CarDetails from './components/CarDetails';
 
 function App() {
+  const name = "Milena";
+  const userName = useState(" Cleidson Ramos ");
+
   return (
     <div className="App">
         <h1>Iniciando a seção 3</h1>
@@ -20,6 +26,14 @@ function App() {
           <ManageData />
           <ListRender />
           <ConditionalRender />
+          {/* props */}
+          <ShowUserName name={userName} />
+          {/* destucturing */}
+          <CarDetails brand="VW" km={70000} color="Vermelho" />
+          {/* reaproveitando */}
+          <CarDetails brand="Ford" km={0} color="Cinza" newCar={true} />
+          <CarDetails brand="Fiat" km={6000} color="Branco" newCar={true} />
+          <CarDetails brand="Chevrolet" km={95000} color="Azul" newCar={false} />
         </div>
     </div>
   );
