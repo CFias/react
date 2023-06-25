f8311615e4d7b6839520135a18207b6133323c25
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-25 14:47:56 -0300

    Upadate ShowUserName.js
    Debug

--- 3_SECTION3/section3/src/components/ShowUserName.js
+++ 3_SECTION3/section3/src/components/ShowUserName.js
@@ -1,7 +1,7 @@
 const ShowUserName = (props) => {
   return (
     <div>
-      <h2>O nome do usuário é: {props.name}</h2>
+      <h2>O nome do usuário é: {props.userName}</h2>
     </div>
   );
 };
