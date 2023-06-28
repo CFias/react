57edb0d9565b1bc0750ccc7d44dfb0b3905506e4
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-28 18:11:10 -0300

    Update App.js
    New object

--- challengecss/challengecss/src/App.js
+++ challengecss/challengecss/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import CarDetails from './components/CarDetails';
 
 function App() {
+  const myCars = [ 
+    { id: 1, name: "Elantra", km: 10000, color: "Branca" },
+    { id: 2, name: "Ka", km: 5700, color: "Cinza Moscou" },
+    { id: 3, name: "Edge", km: 0, color: "Preto" }
+  ];
+
   return (
     <div className="App">
-      <h1>Challenge</h1>
+      <h1>Shwroom de carros</h1>
+      <div className="car-container">
+          {myCars.map(( car ) => (
+            <CarDetails key={car.id} car={car}/>
+          ))}
+      </div>
     </div>
   );
 }
