bf710dcd0af97f6f4acbf69f59b3ac1b5d3488fb
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-28 10:37:10 -0300

    Create MyComponent.js

---
+++ 4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./MyComponent.css"
+
+const MyComponent = () => {
+  return (
+    <div>
+        <p>Este parágrafo é do componente</p>
+        <p className="my-comp-paragraph">Este também é do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent
