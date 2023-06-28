8f06446004cd3f5e90f3bd3e24abb46a5004d17c
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-28 11:04:29 -0300

    Create CarDetails.js

---
+++ challengecss/challengecss/src/components/CarDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CarDetails = () => {
+
+  return (
+    <div>
+        <div>
+            <h2>Ford</h2>
+            <p>Co</p>
+        </div>
+    </div>
+  )
+
+}
+
+export default CarDetails
