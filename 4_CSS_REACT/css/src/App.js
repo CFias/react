e7cc9d5e119894ea2b04980ecae53bdb9ce6e7db
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-28 10:36:38 -0300

    Update App.js

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,9 +1,50 @@
+import { useState } from 'react';
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
+  const n = 15;
+
+  const [name] = useState("Fias");
+
+  const redTitle = false;
+
   return (
     <div className="App">
+      {/* CSS Global */}
       <h1>React com CSS</h1>
+      {/* CSS de Componente */}
+      <MyComponent />
+      <p>Este parágrafo é do app.js</p>
+      {/* Inline CSS */}
+      <p
+        style={{
+          color: "magenta",
+          padding: "25px",
+          borderTop: "5px solid red"
+        }}>
+        Este parágrafo tem o estilo inline
+      </p>
+      <p
+        style={{
+          color: "magenta",
+          padding: "25px",
+          borderTop: "5px solid red"
+        }}>
+        Este parágrafo tem o estilo inline
+      </p>
+      {/* CSS Inline dinâmico */}
+      <h2 
+      style={n < 10 ? { color: "red" } : { color: "black" }}>CSS Dinâmico
+      </h2>
+      <h2 
+      style={n > 10 ? { color: "red" } : { color: "black" }}>CSS Dinâmico
+      </h2>
+      <h2 style={name === "Fias" ? { color: "grey", backgroundColor: "gold" } : { color: "blue" }}>Teste nome</h2>
+      {/* Class dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este estilo vai ter uma classe dinâmica  
+      </h2>
     </div>
   );
 }
