801679893932ae21dcb9929ecc65b206188fdb23
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-29 23:10:49 -0300

    Create New Project

---
+++ 5_FORM_EM_REACT/form/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyForms from './components/MyForms';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Forms</h2>
+      <MyForms />
+    </div>
+  );
+}
+
+export default App;
