0753d4a8f3c324617cb6c93ebf2580941708f9aa
Cleidson Fias <cleidson.fias@hotmail.com>
2023-07-01 00:10:41 -0300

    Update MyForms.js
    Event onChange

--- 5_FORM_EM_REACT/form/src/components/MyForms.js
+++ 5_FORM_EM_REACT/form/src/components/MyForms.js
@@ -1,21 +1,41 @@
 import "./MyForms.css"
+import { useState } from "react";
 
 const MyForms = () => {
+  // 3 - gerenciamento de dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  console.log(name);
+  console.log(email);
+
   return (
     <div className="container">
       {/* 1 - criação de form */}
-        <form>
-            <div>
-                <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeHolder="Digite seu nome aqui" />
-            </div>
-            {/* 2 - label envolvendo input */}
-            <label>
-              <span>E-mail</span>
-              <input type="email" name="email" placeHolder="Digite aqui o seu e-mail" />
-            </label>
-            <input type="submit" value="enviar" />
-        </form>
+      <form>
+        <div>
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome"
+            onChange={handleName} />
+        </div>
+        {/* 2 - label envolvendo input */}
+        <label>
+          <span>E-mail</span>
+          <input 
+          type="email" 
+          name="email" 
+          placeholder="Digite aqui o seu e-mail" 
+          onChange={(e) => setEmail(e.target.value)}/>
+        </label>
+        <input type="submit" value="enviar" />
+      </form>
     </div>
   )
 }
