c50954dd2c683df4b31cf416592011a2c2ad40dc
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-30 23:50:37 -0300

    Update MyForms.js

--- 5_FORM_EM_REACT/form/src/components/MyForms.js
+++ 5_FORM_EM_REACT/form/src/components/MyForms.js
@@ -3,11 +3,17 @@ import "./MyForms.css"
 const MyForms = () => {
   return (
     <div className="container">
+      {/* 1 - criação de form */}
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
                 <input type="text" name="name" placeHolder="Digite seu nome aqui" />
             </div>
+            {/* 2 - label envolvendo input */}
+            <label>
+              <span>E-mail</span>
+              <input type="email" name="email" placeHolder="Digite aqui o seu e-mail" />
+            </label>
             <input type="submit" value="enviar" />
         </form>
     </div>
