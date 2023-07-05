9ff55ba90f963c66521d64212fcc9c5a6680dd94
Cleidson Fias <cleidson.fias@hotmail.com>
2023-07-05 00:01:50 -0300

    Update App.js
    News stages

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -23,22 +23,62 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickdWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickedWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words)
+    const category = 
+      categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    console.log(category)
+
+    // pick a random word
+    const word =
+     words[category][Math.floor(Math.random() * words[category].length)]
+
+     console.log(word)
+
+     return { word, category };
+  };
 
   // Start the secret word game
   const startGame = () => {
+    // pick word and pick category
+    const { word, category } = pickedWordAndCategory();
+
+    // create an array of letters
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   };
 
   // Process the letter input
   const verifyLetter = () => {
-    setGameStage(stages[2].name)
-  }
+    setGameStage(stages[2].name);
+  };
+
+  // Restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
 
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={ startGame } />}
       {gameStage === "game" && <Game verifyLetter={ verifyLetter } />}
-      {gameStage === "end" && <GameOver />}
+      {gameStage === "end" && <GameOver retry={ retry } />}
     </div>
   );
 }
