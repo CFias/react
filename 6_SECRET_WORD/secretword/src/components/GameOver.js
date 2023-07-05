88aacd1e3d0582b33cff210afce91e3c595cbb3a
Cleidson Fias <cleidson.fias@hotmail.com>
2023-07-05 00:02:19 -0300

    Update GameOver.js
    Att component

--- 6_SECRET_WORD/secretword/src/components/GameOver.js
+++ 6_SECRET_WORD/secretword/src/components/GameOver.js
@@ -1,9 +1,12 @@
 import "./GameOver.css"
 
-const GameOver = () => {
+const GameOver = ({ retry }) => {
   return (
-    <div>GameOver</div>
-  )
-}
+    <div>
+      <h1>GameOver</h1>
+      <button onClick={ retry }>Resetar jogo</button>
+    </div>
+  );
+};
 
-export default GameOver
+export default GameOver;
