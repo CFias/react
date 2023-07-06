1a44874ac6c0b07fd5c661f954148d051fa81c80
Cleidson Fias <cleidson.fias@hotmail.com>
2023-07-05 23:31:19 -0300

    Update words.js
    Test

--- 6_SECRET_WORD/secretword/src/data/words.js
+++ 6_SECRET_WORD/secretword/src/data/words.js
@@ -6,3 +6,15 @@ export const wordsList = {
     programação: ["Linguagem", "Framework", "JavaScript", "React"],
     alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
 };
+
+export function response( input ) {
+    //Insert your code here 
+    const response = []; 
+   for (var i=1; i <= input; i++){
+       if (i % 15 == 0) response.push("FizzBuzz");
+       else if (i % 3 == 0) response.push("Fizz");
+       else if (i % 5 == 0) response.push("Buzz");
+       else response.push(i);
+   }
+   return response;
+}
