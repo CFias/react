5d8e3fd9bbda8bdaab228eaae62e9e5c31c129f9
Cleidson Fias <cleidson.fias@hotmail.com>
2023-07-08 22:12:45 -0300

    Update words.js
    Organization

--- 6_SECRET_WORD/secretword/src/data/words.js
+++ 6_SECRET_WORD/secretword/src/data/words.js
@@ -1,6 +1,6 @@
 export const wordsList = {
     carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-    fruta: ["Banana", "Maça", "Pêra" , "Mamão", "Laranja"],
+    fruta: ["Banana", "Maça", "Pêra", "Mamão", "Laranja"],
     corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
     computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
     programação: ["Linguagem", "Framework", "JavaScript", "React"],
