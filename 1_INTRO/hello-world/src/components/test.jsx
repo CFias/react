f1ec8bb4827f52062b662cb730edf583980042b9
Cleidson Ramos Fias <cleidson.fias@hotmail.com>
2023-09-02 23:20:08 -0300

    Update test.jsx

--- 1_INTRO/hello-world/src/components/test.jsx
+++ 1_INTRO/hello-world/src/components/test.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 export default function Testes() {
     const [nome, setNome] = useState("Cleidson Fias");
 
-
   return (
     <div>
         <input value="test" type="text" placeholder='Digite o seu nome'/>
