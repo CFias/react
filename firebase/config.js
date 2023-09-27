4630e85fdaf4de19484986bca7654909d8c1cb5d
Cleidson Ramos Fias <cleidson.fias@hotmail.com>
2023-09-26 22:28:58 -0300

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAPnpLAeq7mgac8ARpeoEOToHPcxu21pj0",
@@ -9,11 +8,10 @@ const firebaseConfig = {
   storageBucket: "blog-carine.appspot.com",
   messagingSenderId: "427065831093",
   appId: "1:427065831093:web:493285d224c0243bcc18fc",
-  measurementId: "G-SHR2PGN3M5"
+  measurementId: "G-SHR2PGN3M5",
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const db = getFirestore(app);
 
