f231d0e17e4a056bc6537af3b8c5a57850d4d3d8
Cleidson Ramos Fias <cleidson.fias@hotmail.com>
2023-10-23 19:54:42 -0300

    fix: Add new file

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,21 +1,21 @@
-import './App.css';
+import "./App.css";
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 // 4 - custom hook
-import { useFetch } from './hooks/useFetch';
+import { useFetch } from "./hooks/useFetch";
 
 const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
-  
+
   // 4 - custom data
   const { data: items } = useFetch(url);
-  
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
-  
+
   // 1 - resgatando dados
   /* 
   useEffect(() => {
@@ -31,59 +31,59 @@ function App() {
     fetchData();
   }, []);
   */
- 
- // 2 - add de produtos
- const handleSubmit = async (e) => {
-   e.preventDefault();
-   
-   
-   const product = {
-     name,
-     price,
+
+  // 2 - add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
     };
-    
+
     const res = await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(product)
+      body: JSON.stringify(product),
     });
-    
+
     // 3 - carregamento dinâmico
     const addedProduct = await res.json();
-    
+
     setProducts((prevProducts) => [...prevProducts, addedProduct]);
-    
+
     setName("");
     setPrice("");
-    
   };
-  
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
       <ul>
-        {items && items.map((product) => (
-          <li key={product.id}>
-            {product.name} - R$ {product.price}
-          </li>
-        ))}
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$ {product.price}
+            </li>
+          ))}
       </ul>
-      <div className='add-product'>
+      <div className="add-product">
         <form onSubmit={handleSubmit}>
           <label>
             Nome:
-            <input 
-              type="text" 
-              value={name} 
-              name='name' 
+            <input
+              type="text"
+              value={name}
+              name="name"
               onChange={(e) => setName(e.target.value)}
-              />
-            <input 
-              type="number" 
-              value={price} 
-              name='price' 
+            />
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name="price"
               onChange={(e) => setPrice(e.target.value)}
             />
           </label>
@@ -92,6 +92,6 @@ function App() {
       </div>
     </div>
   );
-};
+}
 
 export default App;
---
+++ test.py
@@ -0,0 +1,32 @@
+import random
+
+def choice_player():
+    choice_p = input("Escolja Pedra, Papel ou Tesoura: ").lower()
+    if choice_p in ['Pedra', 'Papel', 'Tesoura']:
+        return choice_p
+    else:
+        print("Escolha inválida. Tente novamente!")
+        return choice_player()
+
+def choice_computer():
+    choice_c = ['Pedra', 'Papel', 'Tesoura']
+    return random.choice(choice_c)
+
+def player_win(player, computer):
+    if player == computer:
+        return "Empate!"
+    elif (player == 'Pedra' and computer == 'Tesoura') or (player == 'Tesoura' and computer == 'Papel') or (player == 'Papel' and computer == 'Pedra'):
+        return 'Você venceu!'
+    else:
+        return "A máquina venceu!"
+
+def game():
+    while True:
+        player = choice_player()
+        computer = choice_computer()
+
+        print(f'Você venceu {player}')
+        print(f'A máquina escolheu {computer} {player}')
+
+        result = player_win(player, computer)
+        print(result)
