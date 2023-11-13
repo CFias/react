5258e31af785954b6daace5f704aa05bb86d5d4b
Cleidson Ramos Fias <116103790+CFias@users.noreply.github.com>
2023-11-13 20:56:09 -0300

    Update CarDetails.js

--- 3_SECTION3/section3/src/components/CarDetails.js
+++ 3_SECTION3/section3/src/components/CarDetails.js
@@ -1,16 +1,17 @@
 const CarDetails = ({ brand, km, color, newCar }) => {
   return (
     <div>
-        <h2>Detalhes do carro</h2>
-        <ul>
-            <li>key={CarDetails.id}</li>
-            <li>Marca: {brand}</li>
-            <li>KM: {km}</li>
-            <li>Cor: {color}</li>
-            <li>Novo: {newCar}</li>
-        </ul>
-        {newCar  && <p>carro novo!</p>}
+      <h2>Detalhes do carro</h2>
+      <ul>
+        <li>key={CarDetails.id}</li>
+        <li>Marca: {brand}</li>
+        <li>KM: {km}</li>
+        <li>Cor: {color}</li>
+        <li>Novo: {newCar}</li>
+      </ul>
+      {newCar && <p>carro novo!</p>}
     </div>
-)};
+  );
+};
 
 export default CarDetails;
